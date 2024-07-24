063e1e6c531586108c74073437b964b31df86aa5
Tola  Buluro <tbuluro@yahoo.com>
2024-07-24 19:59:35 +0000

    media css and javascript event listner code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
-function runQuiz () {
+let inputs = document.querySelector(".data"),
+resetBtn = document.querySelector(".btn-reset");
+submitBtn = document.querySelector(".btn-answer");
+question = document.querySelector(".question span"),
 
+/* getting random words from wordList*/
+function randomWord() {
+    let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
+    //random word objects
+    let word = ranObj.word;
+    console.log(ranObj);
+
+    question.innerText = ranObj.question;
+
+    let html ="";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type = "text" disabled>`;
+    }
+    inputs.innerHTML = html;
 }
-
-function checkAnswers () {
-
-}
-
-function solveTheQuiz () {
-
-}
-
-function incrementScore () {
-
-}
-
-function incrementwrongAnswer () {
-    
-}
+    resetBtn.addEventListener("click", randomWord);

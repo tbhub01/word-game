53fd814f866f107a753a7b34199900e47db9494c
Tola  Buluro <tbuluro@yahoo.com>
2024-07-24 21:12:58 +0000

    updated javascript with check answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,28 @@ function randomWord() {
   currentWord = ranObj;
   question.innerText = ranObj.question;
 
-  let inputHTML = Array.from({ length: ranObj.word.length }, () => `<input type="text" disabled>`).join("");
+  let inputHTML = `<input type="text" id="answer" value="">`;
   inputElements.innerHTML = inputHTML;
 }
 
 randomWord();
 resetBtn.addEventListener("click", randomWord);
 
+function checkAnswer() {
+    let userAnswer = document.getElementById("answer").value.toLowerCase();
+    let correctAnswer = currentWord.word.toLowerCase();
+
+    if (userAnswer === correctAnswer) {
+        alert("Correct!");
+        correctCount++;
+        document.getElementById("correct").innerText = correctCount;
+    } else {
+        alert("Incorrect. The correct answer is " + correctAnswer);
+        wrongCount++;
+        document.getElementById("wrong").innerText = wrongCount;
+    }
+
+    randomWord();
+}
+
+submitBtn.addEventListener("click", checkAnswer);
